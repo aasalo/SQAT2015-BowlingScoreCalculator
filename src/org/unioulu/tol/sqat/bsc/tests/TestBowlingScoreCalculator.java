28330c2932d0d7c71eef29515c85d7b4f16a2d73
somename <someemail>
2015-10-16 12:25:34 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat/bsc/tests/TestBowlingScoreCalculator.java
+++ src/org/unioulu/tol/sqat/bsc/tests/TestBowlingScoreCalculator.java
@@ -39,5 +39,36 @@ public class TestBowlingScoreCalculator {
 		assertEquals(6, frame.score());
 		
 	}
+	
+	@Test
+	public void testGameWithTenFrames(){
+		//Arrange
+		BowlingGame game = new BowlingGame();
+		
+		//Act
+		Frame frame1 = new Frame(1, 5);
+		Frame frame2 = new Frame(3, 6);
+		Frame frame3 = new Frame(7, 2);
+		Frame frame4 = new Frame(3, 6);
+		Frame frame5 = new Frame(4, 4);
+		Frame frame6 = new Frame(5, 3);
+		Frame frame7 = new Frame(3, 3);
+		Frame frame8 = new Frame(4, 5);
+		Frame frame9 = new Frame(8, 1);
+		Frame frame10 = new Frame(2, 6);
+		game.addFrame(frame1);
+		game.addFrame(frame2);
+		game.addFrame(frame3);
+		game.addFrame(frame4);
+		game.addFrame(frame5);
+		game.addFrame(frame6);
+		game.addFrame(frame7);
+		game.addFrame(frame8);
+		game.addFrame(frame9);
+		game.addFrame(frame10);
+		
+		//Assert
+		game.frames.size();
+	}
 
 }
