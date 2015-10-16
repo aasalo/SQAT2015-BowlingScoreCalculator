b87df8387734f38325bbebb5d6a7f47b9bb189b2
somename <someemail>
2015-10-16 12:38:48 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat/bsc/tests/TestBowlingScoreCalculator.java
+++ src/org/unioulu/tol/sqat/bsc/tests/TestBowlingScoreCalculator.java
@@ -26,7 +26,7 @@ public class TestBowlingScoreCalculator {
 	@Test
 	public void testComputeScoreOfaFrame(){
 		//Arrange
-		BowlingGame game = new BowlingGame();
+		//BowlingGame game = new BowlingGame();
 		
 		int throw1 = 2;
 		int throw2 = 4;
@@ -71,5 +71,37 @@ public class TestBowlingScoreCalculator {
 		//Assert
 		assertEquals(10, game.frames.size());
 	}
-
+	
+	@Test
+	public void testComputeGameScore(){
+		//Arrange
+		BowlingGame game = new BowlingGame();
+		Frame frame1 = new Frame(1, 5);
+		Frame frame2 = new Frame(3, 6);
+		Frame frame3 = new Frame(7, 2);
+		Frame frame4 = new Frame(3, 6);
+		Frame frame5 = new Frame(4, 4);
+		Frame frame6 = new Frame(5, 3);
+		Frame frame7 = new Frame(3, 3);
+		Frame frame8 = new Frame(4, 5);
+		Frame frame9 = new Frame(8, 1);
+		Frame frame10 = new Frame(2, 6);
+		
+		//Act
+		game.addFrame(frame1);
+		game.addFrame(frame2);
+		game.addFrame(frame3);
+		game.addFrame(frame4);
+		game.addFrame(frame5);
+		game.addFrame(frame6);
+		game.addFrame(frame7);
+		game.addFrame(frame8);
+		game.addFrame(frame9);
+		game.addFrame(frame10);
+		
+		
+		//Assert
+		assertEquals(81, game.score());
+		
+	}
 }
